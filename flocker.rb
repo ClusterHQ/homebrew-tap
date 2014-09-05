79c5a0fb466a4519c1bcf05165de1f0954ede3c8
Richard Wall <richard.wall@clusterhq.com>
2014-09-05 10:27:00 +0100

    Add a head section to install directly from GitHub and add some comments

--- flocker.rb
+++ flocker.rb
@@ -3,14 +3,23 @@ require "formula"
 class Flocker < Formula
   homepage "https://clusterhq.com"
 
+  # Installed by default.
   stable do
     url "https://storage.googleapis.com/archive.clusterhq.com/downloads/flocker/Flocker-0.1.0.tar.gz"
     sha1 "59f61aa85b44981fc3ea8ddb132628b9ee27d02e"
   end
 
+  # Installed with ``brew install --devel``
+  # Put weekly releases here.
   devel do
     url "https://storage.googleapis.com/archive.clusterhq.com/downloads/flocker/Flocker-0.1.1.tar.gz"
     sha1 "fefe853683c717660d7437fa78ab85b032875c38"
+  end
+
+  # Installed with ``brew install --head``
+  # Installs the master branch directly from GitHub.
+  head do
+    url "https://github.com/ClusterHQ/flocker.git"
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
