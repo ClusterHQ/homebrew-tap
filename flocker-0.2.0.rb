ca309df485907994c611d82d413ea4649abac3d4
Adam Dangoor <adamdangoor@gmail.com>
2014-09-30 12:04:21 +0100

    Start of 0.2.0 recipe (without SHA1)

---
+++ flocker-0.2.0.rb
@@ -0,0 +1,22 @@
+require "formula"
+
+class Flocker014 < Formula
+  homepage "https://clusterhq.com"
+  url "https://storage.googleapis.com/archive.clusterhq.com/downloads/flocker/Flocker-0.2.0.tar.gz"
+  sha1 "TODO"
+  depends_on :python if MacOS.version <= :snow_leopard
+
+  def install
+    ENV.prepend_create_path "PYTHONPATH", lib/"python2.7/site-packages"
+
+    system "python", "setup.py", "install",
+           "--prefix=#{prefix}",
+           "--install-scripts=#{bin}",
+           "--install-data=#{libexec}"
+    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+
+  test do
+    system "#{bin}/flocker-deploy", "--version"
+  end
+end
